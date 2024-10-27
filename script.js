8d735bcf59e46563eaa6e0d3189dedcf9bc691b0
Shrutika <shrutikadayalkar1@gmail.com>
2024-10-27 14:10:16 +0530

    add

---
+++ script.js
@@ -0,0 +1,23 @@
+// Script for navigation bar
+const bar=document.getElementById('bar');
+const close =document.getElementById('close');
+const nav =document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click' , ()=>{
+        nav.classList.add('active')
+    })
+
+    }
+
+    if(close){
+        close.addEventListener('click' ,()=>{
+            close.classList.remove('active')
+        }
+    
+        )
+    
+        }
+
+
+
